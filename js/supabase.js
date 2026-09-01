3cb699ea6712edc175863cd26d029a37156804b5
Rombaut Valentijn <valentijn.rombaut@onderwijs.gent.be>
2026-09-01 23:14:32 +0200

    SupaBaseOK

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 // supabase.js — Supabase client initialisatie
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-export const SUPABASE_URL = "https://JOUW-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "JOUW-ANON-KEY";
+export const SUPABASE_URL = "https://iriqsfxcrfdnkfvqmirw.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_a_FThAr0HuB5UFdwzCv4og_Lz2lM1nb";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
